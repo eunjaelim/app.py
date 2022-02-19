f119e90de900985e70cba730f4f0d08283535a6f
eunjaelim <99618295+eunjaelim@users.noreply.github.com>
2022-02-19 21:07:16 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
-import requests
-from bs4 import BeautifulSoup
-
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
@@ -11,33 +8,40 @@ db = client.dbsparta
 ## HTML을 주는 부분
 @app.route('/')
 def home():
-   return render_template('index.html')
-
-@app.route('/memo', methods=['GET'])
-def listing():
-    articles = list(db.articles.find({},{'_id':False}))
-    return jsonify({'all_articles':articles})
+    return render_template('index.html')
 
 ## API 역할을 하는 부분
-@app.route('/memo', methods=['POST'])
-def saving():
-    url_receive = request.form['url_give']
-    comment_receive = request.form['comment_give']
+@app.route('/review', methods=['POST'])
+def write_review():
+    title_receive = request.form['title_give']
+    review_receive = request.form['review_give']
+
+    doc = {
+        'title':title_receive,
+        'review':review_receive
+    }
+
+    db.dict.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
 
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
+@app.route('/review', methods=['GET'])
+def read_reviews():
+    reviews = list(db.dict.find({}, {'_id': False}))
+    return jsonify({'all_reviews': reviews})
 
-    soup = BeautifulSoup(data.text, 'html.parser')
 
-    title = soup.select_one('meta[property="og:title"]')['content']
-    image = soup.select_one('meta[property="og:image"]')['content']
-    desc = soup.select_one('meta[property="og:description"]')['content']
+@app.route('/api/delete', methods=['POST'])
+def delete_reviews():
+    title_receive = request.form['title_give']
+    db.dict.delete_one({'title': title_receive})
+    return jsonify({'msg': '삭제 완료!'})
 
-    doc = {'title': title, 'image': image,'desc':desc,'url':url_receive,'comment':comment_receive}
-    db.articles.insert_one(doc)
-    return jsonify({'msg':'저장이 완료되었습니다!'})
+
+
+
+
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
